6a76f926f7bbda7684678fdd2c4340a97962887a
mokastr <69509545+mokastr@users.noreply.github.com>
2023-04-10 19:08:36 +0500

    Использовал React Context для прокидывания props в глубь компонентов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './pages/Home'
@@ -6,17 +6,23 @@ import Cart from './pages/Cart'
 import NotFound from './pages/NotFound'
 import './scss/app.scss'
 
+export const SearchContext = createContext()
+
 function App() {
+	const [searchValue, setSearchValue] = useState('')
+
 	return (
 		<div className="wrapper">
-			<Header />
-			<div className="content">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</div>
+			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
+				<Header />
+				<div className="content">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</div>
+			</SearchContext.Provider>
 		</div>
 	)
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 
 import Categories from '../components/Categories'
 import Sort from '../components/Sort'
 import PizzaBlock from '../components/PizzaBlock'
 import Skeleton from '../components/PizzaBlock/Skeleton'
+import Pagination from '../components/Pagination'
+import { SearchContext } from '../App'
 
 const Home = () => {
+	const { searchValue } = useContext(SearchContext)
 	const [items, setItems] = useState([])
 	const [isLoading, setIsLoading] = useState(true)
 	const [categoryId, setCategoryId] = useState(0)
+	const [currentPage, setCurrentPage] = useState(1)
 	const [sortType, setSortType] = useState({
 		name: 'популярности',
 		sortProperty: 'rating',
@@ -20,16 +24,30 @@ const Home = () => {
 		const sortBy = sortType.sortProperty.replace('-', '')
 		const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
 		const category = categoryId > 0 ? `category=${categoryId}` : ''
+		const search = searchValue ? `&search=${searchValue}` : ''
 
 		fetch(
-			`https://64182e9275be53f451d80d40.mockapi.io/react-pizza-items?${category}&sortBy=${sortBy}&order=${order}}`
+			`https://64182e9275be53f451d80d40.mockapi.io/react-pizza-items?page=${currentPage}&limit=4${category}${search}&sortBy=${sortBy}&order=${order}}`
 		)
 			.then(res => res.json())
 			.then(json => setItems(json))
 			.catch(err => console.warn(err))
 			.finally(() => setIsLoading(false))
 		window.scrollTo(0, 0)
-	}, [categoryId, sortType])
+	}, [categoryId, sortType, searchValue, currentPage])
+
+	// // Для статичного поиска
+	// const pizzas = items
+	// 	.filter(obj => {
+	// 		if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+	// 			return true
+	// 		}
+	// 		return false
+	// 	})
+	// 	.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+
+	const pizzas = items.map(obj => <PizzaBlock key={obj.id} {...obj} />)
+	const skeletons = [...Array(6)].map((_, i) => <Skeleton />)
 
 	return (
 		<div className="container">
@@ -41,11 +59,8 @@ const Home = () => {
 				<Sort value={sortType} onChangeSort={id => setSortType(id)} />
 			</div>
 			<h2 className="content__title">Все пиццы</h2>
-			<div className="content__items">
-				{isLoading
-					? [...Array(6)].map((_, i) => <Skeleton />)
-					: items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-			</div>
+			<div className="content__items">{isLoading ? skeletons : pizzas}</div>
+			<Pagination onChangePage={number => setCurrentPage(number)} />
 		</div>
 	)
 }
